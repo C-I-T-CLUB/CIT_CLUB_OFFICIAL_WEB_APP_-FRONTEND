32f3128ac936a6ba7737b898cb528dbf674ef1cf
kass8mal8 <kassimaly21@gmail.com>
2022-10-31 23:42:45 +0300

    3 files modified

--- src/pages/Gallary.js
+++ src/pages/Gallary.js
@@ -5,13 +5,10 @@ import "../assets/css/slick-theme.css";
 import "../assets/css/slick.css";
 
 /*Import images needed in the carousel*/
-import ev_1 from '../assets/img/ev_1.jpg'
 import ev_2 from '../assets/img/ev_2.jpg'
 import ev_3 from '../assets/img/ev_3.jpg'
-import ev_4 from '../assets/img/ev_4.jpg'
 import ev_5 from '../assets/img/ev_5.jpg'
 import ev_6 from '../assets/img/ev_6.jpg'
-import ev_7 from '../assets/img/ev_7.jpg'
 import ev_8 from '../assets/img/ev_8.jpg'
 
 
@@ -19,14 +16,11 @@ import ev_8 from '../assets/img/ev_8.jpg'
 const Gallery =()=> {
 
   const images = [
-    {src:ev_1,id:1},
-    {src:ev_2,id:2},
-    {src:ev_3,id:3},
-    {src:ev_4,id:4},
-    {src:ev_5,id:5},
-    {src:ev_6,id:6},
-    {src:ev_7,id:7},
-    {src:ev_8,id:8}
+    {src:ev_2,id:1},
+    {src:ev_3,id:2},
+    {src:ev_5,id:3},
+    {src:ev_6,id:4},
+    {src:ev_8,id:5}
   ]
 
   const settings = {
@@ -40,6 +34,9 @@ const Gallery =()=> {
         {
           breakpoint: 800,
           settings: {
+            className:'center',
+            centerMode:true,
+            centerPadding: "160px",
             slidesToShow: 3,
             slidesToScroll: 1,
             infinite: true,
